0715b4b3fee2445c898080187f5f209ef79ba594
iskayanir <iska.yanir@gmail.com>
2024-01-08 22:04:14 +0200

    finished task number 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,76 @@
-import WebsiteScreen from '@/lib/components/website-screen';
-import { COURSE_WELCOME_TEXT } from '@/lib/config';
-import Image from 'next/image';
-import Link from 'next/link';
+'use client';
+
+import React, { useState } from 'react';
 
 export default function Home() {
+  // State variables to track the color changes
+  const [isColorChanged, setColorChanged] = useState(false);
+
+  // Function to toggle background and text color
+  const toggleColor = () => {
+    setColorChanged(!isColorChanged);
+  };
+
+  // Style object to dynamically set background and text color
+  const style = {
+    backgroundColor: isColorChanged ? '#48D1CC' : '#b5a9ff',
+    color: isColorChanged ? '#000000' : '#008000',
+  };
+
   return (
-    <WebsiteScreen>
-      <div className="grid">
-        <div className="center padding-2 text-xl">
-          <span>
-            {COURSE_WELCOME_TEXT}
-          </span>
-        </div>
-        <div className="center padding-2">
-          <Image
-            src="/huji.png"
-            alt="HUJI Logo"
-            width={320}
-            height={100}
-            priority
-          />
-        </div>
-        <div className="center padding-2">
-          <Image
-            src="/bezalel.png"
-            alt="Bezalel Logo"
-            className="item"
-            width={320}
-            height={100}
-            priority
-          />
-        </div>
-        <div>
-          <h1 className="center text-xxxl">
-            Lorem Ipsum
-          </h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec faucibus sit amet nunc nec vehicula. Fusce ornare nec nulla non imperdiet. Fusce vel sodales justo. Sed efficitur arcu lorem, at iaculis odio ultricies et. Duis rutrum urna nec elit bibendum, sed hendrerit nulla posuere. Vestibulum vestibulum, ante non tincidunt posuere, dui arcu lacinia nisl, nec rhoncus massa arcu ac ipsum. Nam congue interdum tortor, eu dignissim massa scelerisque vitae. Sed ultricies bibendum congue. Praesent non magna id ligula maximus luctus. Donec vitae nibh quis neque luctus sagittis et eget nunc. Aliquam id ullamcorper lacus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur tempor quis dolor a lacinia.
-          </p>
-          <ul>
-            <li>Aliquam maximus tellus sed lacus venenatis, ac cursus eros mollis.</li>
-            <li>In id ante sed sem pharetra molestie et vitae arcu.</li>
-            <li>Cras pharetra turpis at pretium elementum.</li>
-            <li>Donec ultrices felis vel lectus auctor iaculis.</li>
-          </ul>
-          <p>
-            See our <Link target="_blank" href="https://github.com/digital-product-jam-2024/course/tree/main/reference">Reference Material</Link> on <Link target="_blank" href="https://github.com/digital-product-jam-2024/course/blob/main/reference/html.md">HTML</Link>, <Link target="_blank" href="https://github.com/digital-product-jam-2024/course/blob/main/reference/css.md">CSS</Link>, and <Link target="_blank" href="https://github.com/digital-product-jam-2024/course/blob/main/reference/javascript.md">JavaScript</Link>.
-          </p>
-        </div>
+    <div className="grid" style={style}>
+      <div className="center padding-2 text-xl">
+        <span>
+        <h1 style={{ fontSize: '60px' }}>Iska's page</h1>
+        <br></br>hi, this is me and my dog, Arty
+        </span>
       </div>
-    </WebsiteScreen>
-  )
+
+      <div className="center padding-2">
+        <img src="arty and me.jpg" alt="arty and me" width="320" height="300" />
+      </div>
+
+      <div className="center padding-2 text-xl">
+        <span>
+        <p>
+          <em>Watch </em>
+          <a href="https://www.youtube.com/shorts/366JnA5GDmI">
+            <em> Arty playing with a snail</em>
+          </a>
+        </p>
+        <p>Arty likes a lot of plays:</p>
+        <ul>
+          <li>run with other dogs</li>
+          <li>bite everything she can</li>
+          <li>pull a rope</li>
+        </ul>
+        </span>
+      </div>
+
+      <div className="center padding-2 text-xl">
+        <p>
+          <strong>A funny video about dogs:</strong>
+        </p>
+      </div>
+
+      <div className="center padding-2">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/UPWd7UL7WvQ?si=1veEp7IACCQZG_24"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+      </div>
+
+      {/* Button to toggle background and text color */}
+      <div className="center padding-2">
+        <button onClick={toggleColor}>
+          {isColorChanged ? 'Restore Color' : 'Change Color'}
+        </button>
+      </div>
+    </div>
+  );
 }
